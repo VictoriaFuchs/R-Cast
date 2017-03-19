4bcfbe68b070d21563c75f264e02792716e69a51
VictoriaFuchs <VicFuchs@gmail.com>
2017-03-19 08:49:04 -0400

    Added Objects
    Added objects in GUI to ViewControl.m

--- R-Cast/R-Cast/ViewController.h
+++ R-Cast/R-Cast/ViewController.h
@@ -11,5 +11,73 @@
 @interface ViewController : NSViewController
 
 
+//----------------------------------------------------------------------------------------------//
+// ||||||||||||||||||||||||||||||||||||||||||   LAN   ||||||||||||||||||||||||||||||||||||||||| //
+//----------------------------------------------------------------------------------------------//
+
+// This is the "LAN" panel
+
+////    These are the elements found under the "Available Devices" subpanel.
+
+@property (strong) IBOutlet NSTextFieldCell *ButtonAvailable;
+
+@property (strong) IBOutlet NSButtonCell *ButtonRefresh;
+
+@property (strong) IBOutlet NSButtonCell *ButtonSearch;
+
+@property (strong) IBOutlet NSButtonCell *ButtonConnect;
+
+@property (strong) IBOutlet NSButtonCell *ButtonRemember;
+
+
+////    These are the elements found under the "Toggle Available Services" subpanel.
+
+@property (strong) IBOutlet NSSegmentedControl *SwitchLAN;
+
+@property (strong) IBOutlet NSSegmentedControl *SwitchWLAN;
+
+@property (strong) IBOutlet NSSegmentedControl *SwitchBT;
+
+@property (strong) IBOutlet NSSegmentedControl *SwitchVPN;
+
+
+//----------------------------------------------------------------------------------------------//
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| //
+//----------------------------------------------------------------------------------------------//
+
+
+
+//----------------------------------------------------------------------------------------------//
+// ||||||||||||||||||||||||||||||||||||||||||   I|O   ||||||||||||||||||||||||||||||||||||||||| //
+//----------------------------------------------------------------------------------------------//
+
+// This is the "I|O" panel
+
+////    These are the elements found under the "Monitors | Audio Devices" subpanel.
+
+@property (strong) IBOutlet NSSegmentedControl *SwitchMonitor;
+
+@property (strong) IBOutlet NSSegmentedControl *SwitchWebcam;
+
+@property (strong) IBOutlet NSSegmentedControl *SwitchAudioOut;
+
+@property (strong) IBOutlet NSSegmentedControl *SwitchAudioIn;
+
+
+////    These are the elements found under the "Peripherals | Input Devices" subpanel.
+
+@property (strong) IBOutlet NSBox *SwitchMouse;
+
+@property (strong) IBOutlet NSBox *SwitchKeyboard;
+
+@property (strong) IBOutlet NSBox *SwitchUSB;
+
+@property (strong) IBOutlet NSBox *SwitchPrint;
+
+
+//----------------------------------------------------------------------------------------------//
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| //
+//----------------------------------------------------------------------------------------------//
+
 @end
 
